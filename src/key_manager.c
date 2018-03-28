77050db9c285117abeb56d8b93463ad9dc221062
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2018-03-28 00:01:42 -0400

    trimed functions and merged it

--- src/key_manager.c
+++ src/key_manager.c
@@ -10,152 +10,156 @@
 
 #define  BUFF_SIZE   1024
 
+int is_valid_githubid(char *githubId);
+int auth_passphrase(char *PGP_passphrase, char *mygithubId);
+int to_exist_publickey(char *githubId);
+int register_pubkey(char *githubId);
+int register_private_key(char *privfile);
+char *get_pubkeyID(char *githubId);
+int auth_user(char *githubId);
+int onion_register_all_pubkey();
 
-// [TODO] public key들이 저장되는 tmp폴더를 따로 만들기. 
-
-
-
-// download [githubId.pub] from github repository.
-// developer : hansh09
-int download_pubkey(char *githubId){
-        char *url = "https://raw.githubusercontent.com/KAIST-IS521/2018-Spring/master/IndividualKeys/";
-		char cmd[BUFF_SIZE];
-		char githubIdpub[100];
-		int ret;
-        snprintf(cmd, BUFF_SIZE, "wget %s%s.pub 2>/dev/null", url, githubId);
-        snprintf(githubIdpub, BUFF_SIZE,"%s.pub", githubId);
-		ret = access(githubIdpub, F_OK);
-		if(ret) // 존재하지 않는다면 다운받는다
-		{
-			system(cmd);
-		}
-	return 0;	
+int is_valid_githubid(char *githubId){
+	char *url = "https://github.com/KAIST-IS521/2018-Spring/blob/master/IndividualKeys/";
+	char cmd[BUFF_SIZE];
+	snprintf(cmd, BUFF_SIZE, "wget --spider --timeout=1 %s%s.pub 2>/dev/null", url, githubId);
+	if(!system(cmd)) return 1; //valid
+	return 0;
 }
 
+// Check whether [PGP_passphrase] string is  match with PGP private key(which is registered in the machine) or not.
+int  auth_passphrase(char *PGP_passphrase, char *mygithubId){
+    char cmd[BUFF_SIZE];
+    char *pub_key ;
+	int auth;
+	
+	if(!to_exist_publickey(mygithubId)) {
+		printf("error! cannot get %s.pub...\n",mygithubId); 
+	}
+	register_pubkey(mygithubId);
+	pub_key =(char*)get_pubkeyID(mygithubId); 
+	if(!pub_key) return 0;
+	
+	system("gpg --help > dummyfile.tmp");
+    snprintf(cmd, BUFF_SIZE, "sudo gpg -r %s --encrypt dummyfile.tmp", pub_key);
+    system(cmd);
+    snprintf(cmd, BUFF_SIZE, "echo %s | sudo gpg --passphrase-fd 0 -r %s --decrypt dummyfile.tmp.gpg > authresult.tmp",PGP_passphrase, pub_key);
+    system(cmd);
+    system("sudo rm dummyfile.tmp.gpg");
+    system("rm dummyfile.tmp");
 
-// register [githubId.pub]  
-// developer : MincheolSon
+    FILE *f;
+    f = fopen("authresult.tmp", "r");
+    if(fgetc(f) == EOF) auth=0;
+    else auth=1; //valid
+	
+	system("rm authresult.tmp");
+	return auth;
+}
+
+int to_exist_publickey(char *githubId){
+    char *url = "https://raw.githubusercontent.com/KAIST-IS521/2018-Spring/master/IndividualKeys/";
+	char cmd[BUFF_SIZE];
+	char githubIdpub[100];
+	int ret;
+    
+    snprintf(githubIdpub, BUFF_SIZE,"%s.pub", githubId);
+	ret = access(githubIdpub, F_OK);
+	if(ret==0) return 1;
+	else{ // no exist
+		if(is_valid_githubid(githubId)){
+			snprintf(cmd, BUFF_SIZE, "wget %s%s.pub 2>/dev/null", url, githubId);
+			system(cmd);
+			return 1;
+		}
+	}
+	return 0; // no exist both local and server. 
+}
+
 int register_pubkey(char *githubId){
     char cmd[BUFF_SIZE];
-	// prerequisite : [githubId.pub] file. 
-	download_pubkey(githubId);
+	to_exist_publickey(githubId); // prerequisite : [githubId.pub] file. 
     snprintf(cmd, BUFF_SIZE,  "gpg --import %s.pub 2>/dev/null", githubId);
     system(cmd);
 }
 
-
-// register [private key]
-// developer : hansh09
 int register_private_key(char *privfile){
 	char cmd[BUFF_SIZE];
 	snprintf(cmd, BUFF_SIZE, "gpg --allow-secret-key-import --import %s 2>/dev/null", privfile);
 	system(cmd);
 }
 
-
-// get key ID of [githubId.pub] 
-// developer : Dauren
 char *get_pubkeyID(char *githubId){
     char cmd[BUFF_SIZE];
 	int i;
 	FILE *f;
 	char c;
-	char *pubkey_id = (char*)malloc(10);
-	
-	// prerequisite : [githubId.pub] file. 
-	download_pubkey(githubId);
+	char *pubkeyID = (char*)malloc(16);
+	if (!to_exist_publickey(githubId)) return NULL; // prerequisite : [githubId.pub] file. 
     snprintf(cmd, BUFF_SIZE, "sudo gpg %s.pub > KeyId.tmp 2>/dev/null",githubId);
-    //create the file containing the KeyId of user githubId
     system(cmd);
     f = fopen("KeyId.tmp", "r");
-    
-
-    if(fgetc(f) == EOF) //if file is empty it means that user is not registered
-    {
-        printf("User %s is not registered yet\n", githubId);
-        return 0;
-    }
-    // get the user's KeyId from KeyId.tmp file
-    // file format is as following
-    // pub  <size of key>/<key id> <creation date> <user name> <email>
-	//      ex) pub  2048R/EDE8D438 2018-03-22 jiwon choi (Second gpg key....) <jiwon.choi@kaist.ac.kr>
-    // we only need <key id> part
+    if(fgetc(f) == EOF) return NULL;
     for(i=0;;c = fgetc(f))
     {
         if(c == '/')
         {
             while((c=fgetc(f)) != ' ')
-                pubkey_id[i++] = c;
-            pubkey_id[i] = '\0';
+                pubkeyID[i++] = c;
+            pubkeyID[i] = '\0';
             break;
         }
     }
 	snprintf(cmd,BUFF_SIZE,"rm KeyId.tmp");
 	system(cmd);
-	return pubkey_id;
+	return pubkeyID;
 }
 
-
-// verify local machine is githubId's or not.
-// deveopler : Dauren
 int auth_user(char *githubId){ 
     char cmd[BUFF_SIZE];
 	int i;
 	FILE *f;
 	char a[9],c;
-
-	// prerequisite : [githubId.pub] file. 
-	download_pubkey(githubId);
-	
-    // now key id of user is stored in *a
-    snprintf(cmd, BUFF_SIZE, "sudo gpg --export-secret-keys -a %s > privateKey.tmp",get_pubkeyID(githubId));
-
-    // check if the private key of user is stored on this machine
-    // If it is, then it will be saved in the privateKey.tmp. Otherwise,
-    // this command will give warning and privateKey.tmp will be empty
+	char *pubkeyID;
+	to_exist_publickey(githubId);// prerequisite : [githubId.pub] file. 
+	pubkeyID=get_pubkeyID(githubId);
+	if(!pubkeyID) return 0;
+    snprintf(cmd, BUFF_SIZE, "sudo gpg --export-secret-keys -a %s > privateKey.tmp", pubkeyID);
     system(cmd);
     f = fopen("privateKey.tmp","r");
-    if(getc(f) == EOF)
-        return 0;
-    else
-    {
-        snprintf(cmd,BUFF_SIZE,"sudo rm privateKey.tmp"); //delete the privateKey file
-                                                          //before exiting this function
+    if(getc(f) == EOF) return 0;
+    else{
+        snprintf(cmd,BUFF_SIZE,"sudo rm privateKey.tmp"); 
         system(cmd);
         return 1;
     }
 }
 
-
-// [TODO] 잘됨. 하지만 쉘에 메시지가 뜸. 어차피 백그라운드로 돌아갈거라 상관없나...
-// Check whether [PGP_passphrase] string is 
-// match with PGP private key(which is registered in the machine) or not.
-// (If you need mygithubId, you can use it.)
-int  auth_passphrase(char *PGP_passphrase, char *mygithubId)
-{
-    char cmd[BUFF_SIZE];
-    char *pub_key = get_pubkeyID(mygithubId);
-    // first create some random file for encryption
-    system("gpg --help > dummyfile.tmp");
-    // then encrypt this file with mygithubId's public key
-    snprintf(cmd, BUFF_SIZE, "sudo gpg -r %s --encrypt dummyfile.tmp 2>/dev/null", pub_key);
-    system(cmd);
-    // then try to decrypt the encrypted file with mygithubId's private key
-    // in order to do this we must pass mygithubId's passphrase
-    // If this passphrase is correct resultant authresult.tmp will be identical to
-    // the dummyfile.tmp, otherwise authresult.tmp will be empty
-    snprintf(cmd, BUFF_SIZE, "echo %s | sudo gpg --passphrase-fd 0 -r %s --decrypt dummyfile.tmp.gpg > authresult.tmp 2>/dev/null",PGP_passphrase, pub_key);
-    system(cmd);
-    // delete all of the created files to not the waste the memory
-    system("sudo rm dummyfile.tmp.gpg");
-    system("rm dummyfile.tmp");
-
-    FILE *f;
-    f = fopen("authresult.tmp", "r");
-    if(fgetc(f) == EOF)
-        return 0;
-    else
-        return 1;
+int onion_register_all_pubkey(){
+	FILE *fp;
+	char line[100];
+	char *githubID;
+	fp = fopen("OnionUser.db.tmp", "r"); 
+	int i=0;
+	
+	if (fp == NULL) {
+		exit(101);
+	}
+	else{
+		while(!feof(fp)){
+			fgets(line, 100, fp);
+			if(line[0]=='-') break; // [TODO] trim
+			githubID = strchr(line, ' ')+1;
+			githubID = strchr(githubID, ' ')+1;
+			githubID[strlen(githubID)-1] = '\0';
+			printf("%s\n",githubID);
+			
+			to_exist_publickey(githubID);
+			register_pubkey(githubID);
+		}
+		printf("key registered\n");
+	}
+	return 0;
 }
 
 
